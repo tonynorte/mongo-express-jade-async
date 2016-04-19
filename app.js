e7b4398fe932ff3cc47e953bde2f064bc0db7b3b
tonynorte <tonynorte@gmail.com>
2016-04-19 10:43:18 -0600

    minor changes

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var monk = require('monk');
 var db = monk('localhost:27017/bands');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -34,8 +32,6 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
-
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
